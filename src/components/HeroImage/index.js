eab229b66ca128a071537d57b9fa5e84ee17f6d3
rogtang <rogtang@gmail.com>
2021-09-13 19:12:33 -0700

    initial upload to deploy

---
+++ src/components/HeroImage/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import {Wrapper, Content, Text} from './HeroImage.styles'
+import PropTypes from 'prop-types'
+
+export default function HeroImage({image, title, text}) {
+    return (
+       <Wrapper image={image}>
+           <Content>
+               <Text >
+                    <h1>{title} </h1>
+                    <p>{text}</p>
+               </Text>
+           </Content>
+       </Wrapper>
+    )
+}
+
+HeroImage.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string,
+    text: PropTypes.string
+}
